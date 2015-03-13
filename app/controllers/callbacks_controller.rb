992a8658069b89819ce79a5e6d6c7afc93e1bee5
Róbert Terkál <terkalma@gmail.com>
2015-03-13 01:12:20 +0100

    removed everything except oauth

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,11 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    begin
+      @user = User.from_oauth request.env["omniauth.auth"]
+      sign_in_and_redirect @user
+    rescue ActiveRecord::RecordInvalid => e
+      flash['alert'] = e.message
+      redirect_to :new_user_session
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,24 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  validate :has_company_email_address
+
+  def self.from_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  private
+  def has_company_email_address
+    address = Mail::Address.new email
+    unless address.domain == "#{Figaro.env.email_domain}.com"
+      errors.add(:email, "You're not welcomed here with a #{address.domain} email domain.")
     end
   end
 end
