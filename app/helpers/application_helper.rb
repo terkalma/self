f73b2845d52a5d3c303b04f4904f122b70bd6304
Róbert Terkál <terkalma@gmail.com>
2015-03-14 22:57:05 +0100

    added footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def app_title
     Figaro.env.app_title || 'App Name'
   end
+
+  def licence_link
+    url = 'http://www.wtfpl.net/'
+    img_url = 'http://www.wtfpl.net/wp-content/uploads/2012/12/wtfpl-badge-4.png'
+
+    <<-HTML
+      <a href=#{url}> <img src=#{img_url} width="80" height="15" alt="WTFPL"/> </a>
+    HTML
+  end
 end
