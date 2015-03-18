d0e4624039ce4a5db658189271390273d3b8d399
Róbert Terkál <terkalma@gmail.com>
2015-03-18 19:51:32 +0100

    added basic panel

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,5 +1,13 @@
 module WelcomeHelper
   def title
-    "Work on #{@date.asctime}"
+    "Work on #{@date.strftime('%a %b %d %Y')}"
+  end
+
+  def hours_worked
+    distance_of_time_in_words @events.total
+  end
+
+  def event_duration(event)
+    distance_of_time_in_words event.duration
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ActiveRecord::Base
   belongs_to :user_project
   include Aggregate
+
+  def project
+    user_project.project
+  end
+
+  def duration
+    hours.hours + minutes.minutes
+  end
 end
