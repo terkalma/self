fdb2c48ca04dcc88c5dce321b41266842eb3e112
Róbert Terkál <terkalma@gmail.com>
2015-03-19 01:04:33 +0100

    fixes

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -3,15 +3,19 @@ module WelcomeHelper
     "Work on #{@date.strftime('%a %b %d %Y')}"
   end
 
-  def hours_worked
-    distance_of_time_in_words @events.total
-  end
-
   def event_duration(event)
     distance_of_time_in_words event.duration
   end
 
   def project_name(event)
     event.project ? event.project.name : 'Project N/A'
+  end
+
+  def events_at(date=@date)
+    current_user.events.at date
+  end
+
+  def hours_worked(date=@date)
+    distance_of_time_in_words events_at(date).total
   end
 end
