47a35538441e684b0f39786add4dddba32942fd0
Róbert Terkál <terkalma@gmail.com>
2015-03-21 03:18:24 +0100

    ability to add people to the project

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -13,9 +13,23 @@ class Admin::ProjectsController < ApplicationController
 
   def edit
     @project = Project.find_by_slug params[:id]
+    @users_to_add = User.where.not(id: @project.users.pluck(:id))
   end
 
   def update
+    begin
+      project = Project.find_by_slug permitted_params[:slug]
+      project.user_ids += [permitted_params[:user_ids]]
+      flash[:notice] = 'Person successfully added'
+    rescue
+      flash[:alert] = 'Unable to add the person to the project'
+    end
 
+    redirect_to action: :edit, id: permitted_params[:slug]
+  end
+
+  private
+  def permitted_params
+    params.require(:project).permit(:slug, :user_ids)
   end
 end
