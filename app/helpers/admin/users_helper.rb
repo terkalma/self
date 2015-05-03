1e99b585fbe236eb54ce8030ceecb6e891f3a4ab
Róbert Terkál <terkalma@gmail.com>
2015-05-03 05:12:48 +0200

    using view inheritance

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,3 +1,5 @@
 module Admin::UsersHelper
-
+  def resource_name
+    @user.name rescue 'John Doe'
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
 module ApplicationHelper
+  String.class_eval do
+    def to_cc
+      str = self.dup || ''
+      str.gsub!(/[^a-zA-Z ]/,'')
+      str.squish.split(' ').map(&:capitalize).join
+    end
+  end
+
+  def resource_title
+    collection_title.singularize
+  end
+
+  def collection_title
+    controller_name.capitalize
+  end
+
   def alert_class
     alert ? 'bg-danger' : 'hide'
   end
