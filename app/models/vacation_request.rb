b4c21b02a6e7c9308c745f98bd758d6161c86b98
Róbert Terkál <terkalma@gmail.com>
2015-08-07 20:24:23 +0200

    add comments to vacation validations

--- app/models/vacation_request.rb
+++ app/models/vacation_request.rb
@@ -22,6 +22,10 @@ class VacationRequest < ActiveRecord::Base
     paid? ? 'Paid' : 'Unpaid'
   end
 
+  def overlapping_error
+    "This request is overlapping with an existing one (#{vacation_from}-#{vacation_to})"
+  end
+
   private
   def compute_length
     return true unless vacation_from && vacation_to
@@ -60,19 +64,47 @@ class VacationRequest < ActiveRecord::Base
     requests = user.vacation_requests.not_declined
     requests = requests.where.not(id: self.id) if persisted?
 
-    error_msg = ->(r) { "This request is overlapping with an existing one (#{r.vacation_from}-#{r.vacation_to})" }
-
-    if r = requests.where('vacation_from <= ?', vacation_from).where('vacation_to >= ?', vacation_from).first
-      errors.add :vacation_from, error_msg.call(r)
+    if r = overlap_from_right(requests)
+      errors.add :vacation_from, r.overlapping_error
     end
 
-    if r = requests.where('vacation_from <= ?', vacation_to).where('vacation_to >= ?', vacation_to).first
-      errors.add :vacation_to, error_msg.call(r)
+    if r = overlap_from_left(requests)
+      errors.add :vacation_to, r.overlapping_error
     end
 
-    if r = requests.where('vacation_from >= ?', vacation_from).where('vacation_to <= ?', vacation_to).first
-      errors.add :vacation_to, error_msg.call(r)
-      errors.add :vacation_from, error_msg.call(r)
+    if r = container(requests)
+      errors.add :vacation_to, r.overlapping_error
+      errors.add :vacation_from, r.overlapping_error
     end
+  end
+
+  #
+  # Checks if +self+ overlaps from the right at least 1 of the requests from +scope+
+  # [a, b] right overlaps [c, d] if a >= c && a =< d
+  #
+  # returns first object which fulfills the condition
+  #
+  def overlap_from_right(scope)
+    scope.where('vacation_from <= ?', vacation_from).where('vacation_to >= ?', vacation_from).first
+  end
+
+  #
+  # Checks if +self+ overlaps from the left at least 1 of the requests from +scope+
+  # [a, b] right overlaps [c, d] if b >= c && b =< d
+  #
+  # returns first object which fulfills the condition
+  #
+  def overlap_from_left(scope)
+    scope.where('vacation_from <= ?', vacation_to).where('vacation_to >= ?', vacation_to).first
+  end
+
+  #
+  # Checks if +self+ contains at least 1 of the requests from +scope+
+  # [a, b] right overlaps [c, d] if a =< c && b >= d
+  #
+  # returns first object which fulfills the condition
+  #
+  def contained(scope)
+    scope.where('vacation_from >= ?', vacation_from).where('vacation_to <= ?', vacation_to).first
   end
 end
