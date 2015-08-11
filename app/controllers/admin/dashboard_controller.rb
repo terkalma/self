893eb17b768a49255fa10bed50d2e2d408e62507
Róbert Terkál <terkalma@gmail.com>
2015-08-11 17:16:10 +0200

    vacation requests

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,10 @@ module Admin
     add_breadcrumb 'Admin', :admin_dashboard_path
 
     def index
+      if VacationRequest.pending.any?
+        @vacation_requests = VacationRequest.pending.includes :user
+        flash.now[:partial] = 'admin/dashboard/pending_vacations'
+      end
     end
   end
 end
