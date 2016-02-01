719b5ce3cfbb969f01fef2c64144ee33e518ca8e
jancsi311 <jancsihidi311@gmail.com>
2016-02-01 13:12:25 +0200

    status added to feedbacks

--- app/datatables/feedback_data_table.rb
+++ app/datatables/feedback_data_table.rb
@@ -1,4 +1,5 @@
 class FeedbackDataTable < BaseDataTable
+  delegate :dismissed_admin_feedback_path, :resolved_admin_feedback_path, :in_progress_admin_feedback_path, to: :@view
 
   def initialize(view:, relation: nil)
     super view: view, relation:  relation || Feedback.joins(:user)
@@ -8,15 +9,29 @@ class FeedbackDataTable < BaseDataTable
   def map_resource(feedback)
     [
       feedback.comment,
-      feedback.user.email
+      feedback.user.email,
+      feedback.status.to_s.capitalize.sub('_', ' '),
+      (%w(dismissed resolved).include? feedback.status) ? '' : button_to('Dismiss', dismissed_admin_feedback_path(feedback), method: :patch ),
+      choose_button(feedback)
     ]
   end
 
   def header
-    %w[comment users.email]
+    %w[comment users.email status]
   end
 
   def default_sort_column
     'users.email'
+  end
+
+  def choose_button(feedback)
+    case feedback.status
+      when 'pending'
+        button_to('In progress', in_progress_admin_feedback_path(feedback), method: :patch )
+      when 'in_progress'
+        button_to('Resolved', resolved_admin_feedback_path(feedback), method: :patch )
+      else
+        ''
+    end
   end
 end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,7 @@
 class Feedback < ActiveRecord::Base
   belongs_to :user
 
+  enum status: [ :pending, :in_progress, :resolved, :dismissed ].map {|status| [status, status.to_s]}.to_h
+
   validates_presence_of :comment
 end
