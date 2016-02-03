7564c4b04386580b70ddb852e9634780a930db23
jancsi311 <jancsihidi311@gmail.com>
2016-02-03 19:09:56 +0200

    filter added to feedbacks

--- app/controllers/admin/feedbacks_controller.rb
+++ app/controllers/admin/feedbacks_controller.rb
@@ -7,9 +7,11 @@ module Admin
     add_breadcrumb 'Feedbacks', :admin_feedbacks_path
 
     def index
+      relation = params[:statusFilter] == 'all' ? Feedback.joins(:user) : Feedback.joins(:user).where(status: params[:statusFilter])
+
       respond_to do |format|
         format.json do
-          render json: FeedbackDataTable.new(view: view_context)
+          render json: FeedbackDataTable.new(view: view_context, relation: relation)
         end
         format.html { }
       end
