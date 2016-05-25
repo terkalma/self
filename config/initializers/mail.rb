780d65394e99c61a9add0beff6cc6915f295198a
terkalma <terkalma@gmail.com>
2016-05-25 17:11:14 +0300

    config update

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
     address: Figaro.env.SMTP_SERVER,
-    port: 587,
-    enable_starttls_auto: true
+    port: 25,
+    enable_starttls_auto: false
 }
 
 ActionMailer::Base.delivery_method = :smtp
